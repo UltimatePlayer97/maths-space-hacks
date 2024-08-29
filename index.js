923c02e1af25dbce80c49dad97570ccbc4859690
UltimatePlayer97 <gogole.help134@gmail.com>
2024-08-30 09:02:10 +1000

    Added Finding Answer class in index.js and Prettier formatting changes.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-console.log(`
-    HELLO! IF YOU SEE THIS:
-    `);
+console.log(`HELLO! IF YOU SEE THIS`);
 
 console.error(`THE EXTENSION IS WORKING.`);
 
-document.body.style.border = "5px solid red";
+document.body.style.border = '5px solid red';
+
+let findAnswers = document.getElementsByClassName('root_1nic0gy-o_O-active_1s8ur59');
+
+console.log(findAnswers);
